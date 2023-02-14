20b52aa2ade73387eb850f0e0a5c0c4fa1386600
NetDefender <danielsith@hotmail.com>
2023-02-14 20:42:21 +0100

    feat: new created date for user

--- User.cs
+++ User.cs
@@ -5,4 +5,5 @@ public sealed class User
     public string Name { get; set; } = "";
     public string FullName {get;set;} = default!;
     public int Age { get; set; } = 100;
+    public DateTime Created { get; set; }
 }
