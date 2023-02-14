7a94b889de9775f4f882914aa2619e6f3aa1c61e
NetDefender <danielsith@hotmail.com>
2023-02-14 20:13:42 +0100

    fix: default age is 50

--- User.cs
+++ User.cs
@@ -4,5 +4,5 @@ public sealed class User
 {
     public string Name { get; set; } = default!;
     public string FullName {get;set;} = default!;
-    public int Age { get; set; } = 0;
+    public int Age { get; set; } = 50;
 }
